76e88d56397b1c6cdd108d3f835b8a7ab42377a6
RussellLuo <luopeng.he@gmail.com>
2019-09-25 22:46:00 +0800

    Add ScheduleTimer example

--- timingwheel.go
+++ timingwheel.go
@@ -184,8 +184,14 @@ type Scheduler interface {
 }
 
 // ScheduleFunc calls f (in its own goroutine) according to the execution
-// plan scheduled by s.
-// It returns a Timer that can be used to cancel the call using its Stop method.
+// plan scheduled by s. It returns a Timer that can be used to cancel the
+// call using its Stop method.
+//
+// If the caller want to terminate the execution plan halfway, it must
+// stop the timer and ensure that the timer is stopped actually, since in
+// the current implementation, there is a gap between the expiring and the
+// restarting of the timer. The wait time for ensuring is short since the
+// gap is very small.
 //
 // Internally, ScheduleFunc will ask the first execution time (by calling
 // s.Next()) initially, and create a timer if the execution time is non-zero.
